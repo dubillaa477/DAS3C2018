76cd5fc350dc77c839484f4bdf8319ddbfb9b736
dubillaa477 <43178679+dubillaa477@users.noreply.github.com>
2018-10-01 19:16:44 -0600

    lAB 05 push

---
+++ Lab05/Composition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05
+{
+    public class StudentC
+    {
+        Person person = new Person();
+        public string FormatIdAndName(string name, int id) {
+            return person.FormatIdAndName(name, id);
+        }
+    }
+}
--- Lab05/Program.cs
+++ Lab05/Program.cs
@@ -10,6 +10,18 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Inheritance");
+
+
+            Console.ReadKey();
         }
+
+        public static void PrintPerson(Student student) {
+            Console.WriteLine(student.FormatIdAndName("Daniel Ubilla", 1));
+        }
+
+        public static void PrintPerson(StudentC studentC) {
+            Console.WriteLine(studentC.FormatIdAndName("Daniel Ubilla", 1));
+        } 
     }
 }
